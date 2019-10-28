c73d43b4fdfeeddd4a74696356db9e446a727f2c
Petr Slobodyanyuk <support@web-kiborg.ru>
2019-10-28 18:19:50 +0700

    v0.4-beta

--- src/crm/OpenCart.php
+++ src/crm/OpenCart.php
@@ -139,7 +139,7 @@ class OpenCart implements CRM {
     $image = explode('/image/', $data['image']);
     $data['image'] = $image[count($image) - 1];
     unset($image);
-    $STH = $this->db->prepare("INSERT INTO oc_product SET model = :model, sku = :sku, upc = '', ean = '', jan = '', isbn = '', mpn = '', location = '', quantity = :quantity, stock_status_id = :stock_status_id, image = :image, manufacturer_id = 0, price = :price, tax_class_id = 0, status = 1, date_added = current_date, date_modified = current_date");
+    $STH = $this->db->prepare("INSERT INTO oc_product SET model = :model, sku = :sku, upc = '', ean = '', jan = '', isbn = '', mpn = '', location = '', quantity = :quantity, stock_status_id = :stock_status_id, image = :image, manufacturer_id = 1, price = :price, tax_class_id = 0, status = 1, date_added = current_date, date_modified = current_date, date_available = current_date");
     $STH->execute([':model' => $data['code'], ':sku' => $data['guid'], ':quantity' => $data['stock'], ':stock_status_id' => $this->getStockStatus($data['stock']), ':image' => $data['image'], ':price' => $data['price']]);
     $product_id = $this->db->lastInsertId('product_id');
 
@@ -176,11 +176,34 @@ class OpenCart implements CRM {
       if ($add_if_empty) {
         $STH = $this->db->prepare("INSERT INTO oc_category (image, parent_id, top, `column`, `status`, date_added, date_modified) VALUES ('', :parent_id, 1, 1, 1, CURRENT_TIME, CURRENT_TIME)");
         $STH->execute([':parent_id' => $parent_id]);
-        $last_insert_id = $this->db->lastInsertId('category_id');
+        $category_id = $this->db->lastInsertId('category_id');
         $STH = $this->db->prepare("INSERT INTO oc_category_description SET category_id = :category_id, language_id = :language_id, `name` = :category_name, description = '', meta_title = '', meta_description = '', meta_keyword = ''");
-        $STH->execute([':category_id' => $last_insert_id, ':category_name' => $name, ':language_id' => $this->default_language_id]);
+        $STH->execute([':category_id' => $category_id, ':category_name' => $name, ':language_id' => $this->default_language_id]);
 
-        return $last_insert_id;
+        $STH = $this->db->prepare("INSERT INTO oc_category_to_store SET category_id = :category_id, store_id = 0");
+        $STH->execute([':category_id' => $category_id]);
+
+        $level = 0;
+        $STH1 = $this->db->prepare("INSERT INTO oc_category_path SET category_id = :category_id, path_id = :path_id, `level` = :level_data");
+        $STH = $this->db->prepare("SELECT path_id FROM oc_category_path WHERE category_id = :parent_id ORDER BY `level`");
+        $STH->execute(['parent_id' => $parent_id]);
+        while ($row = $STH->fetch(PDO::FETCH_ASSOC)) {
+          $STH1->execute([
+            ':category_id' => $category_id, ':path_id' => $row['path_id'], ':level_data' => $level
+          ]);
+          $level++;
+        }
+        $STH1->execute([
+          ':category_id' => $category_id, ':path_id' => $category_id, ':level_data' => $level
+        ]);
+        unset($STH1, $level, $row);
+
+        $STH = $this->db->prepare("INSERT IGNORE INTO oc_seo_url SET store_id = 0, language_id = :language_id, `query` = :query_data, `keyword` = :keyword_data");
+        $STH->execute([
+          ':language_id' => $this->default_language_id, ':query_data' => 'category_id=' . $category_id, ':keyword_data' => $this->generateSeoUrl($name)
+        ]);
+
+        return $category_id;
       }
       else {
         return 0;
@@ -273,5 +296,20 @@ class OpenCart implements CRM {
     else {
       return $this->stock_statuses['false'];
     }
+  }
+
+  private function generateSeoUrl(string $name): string {
+    $name = strip_tags($name); // убираем HTML-теги
+    $name = str_replace(["\n", "\r"], " ", $name); // убираем перевод каретки
+    $name = preg_replace("/\s+/", ' ', $name); // удаляем повторяющие пробелы
+    $name = trim($name); // убираем пробелы в начале и конце строки
+    $name = function_exists('mb_strtolower') ? mb_strtolower($name) : strtolower($name); // переводим строку в нижний регистр (иногда надо задать локаль)
+    $name = strtr($name, [
+      'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+      'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''
+    ]);
+    $name = preg_replace("/[^0-9a-z-_ ]/i", "", $name); // очищаем строку от недопустимых символов
+    $name = str_replace(" ", "-", $name); // заменяем пробелы знаком минус
+    return $name; // возвращаем результат
   }
 }
