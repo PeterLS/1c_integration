12b4c8345fe6f6e4dd6f23cca0225cbfef83a2c3
Petr Slobodyanyuk <support@web-kiborg.ru>
2019-11-05 13:23:43 +0700

    v0.9-beta

--- src/crm/OpenCart.php
+++ src/crm/OpenCart.php
@@ -127,14 +127,11 @@ class OpenCart implements CRM {
       unset($product_description_data);
 
       if (!empty($data['filters'])) {
-        $STH = $this->db->prepare("INSERT IGNORE INTO oc_product_filter VALUES (:product_id, :filter_id)");
+        $STH = $this->db->prepare("INSERT IGNORE INTO `oc_product_filter` VALUES (:product_id, :filter_id)");
+        $STH1 = $this->db->prepare("INSERT IGNORE INTO `oc_category_filter` SET `category_id` = (SELECT `category_id` FROM `oc_product_to_category` WHERE `product_id` = :product_id) AND `filter_id` = :filter_id");
         foreach ($data['filters'] as $filter_id) {
           $STH->execute([':product_id' => $product_id, ':filter_id' => $filter_id]);
-
-          $this->db->prepare("INSERT IGNORE INTO `oc_category_filter` SET `category_id` = (SELECT `category_id` FROM `oc_product_to_category` WHERE `product_id` = :product_id) AND `filter_id` = :filter_id")->execute([
-            ':filter_id' => $filter_id,
-            ':product_id' => $product_id
-          ]);
+          $STH1->execute([':product_id' => $product_id, ':filter_id' => $filter_id]);
         }
       }
 
